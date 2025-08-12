a67856df87cc1e1a809c671ad87217595301dfb9
Nicolas <68703714+AtypicalAsian@users.noreply.github.com>
2025-08-11 21:45:22 -0700

    LC-minimum-common-value [Runtime: 3 ms; Memory: 62.8 MB]

---
+++ two-pointers/minimum-common-value.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int getCommon(int[] nums1, int[] nums2) {
+        int p1 = 0;
+        int p2 = 0;
+        int minCommon = Integer.MAX_VALUE;
+        while ((p1 < nums1.length && (p2 < nums2.length))){
+            if (nums1[p1] == nums2[p2]){
+                minCommon = Math.min(minCommon,nums1[p1]);
+                p1++;
+                p2++;
+            } else if (nums1[p1] < nums2[p2]){
+                p1++;
+            } else{
+                p2++;
+            }
+        }
+        return minCommon != Integer.MAX_VALUE ? minCommon : -1;
+    }
+}
